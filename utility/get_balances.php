4ad9487875a24591d043b210ad0228759e624809
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-05-01 16:54:59 +0200

    fixed balances

--- utility/get_balances.php
+++ utility/get_balances.php
@@ -2,13 +2,18 @@
 // Connect to the database
 require_once "db_connection.php";
 $user_id = $_SESSION['user_id'];
+$sub_user_id = $_SESSION['sub_user_id'];
 
 // Prepare the SQL query to get the user balance
 $stmt = $conn->prepare("SELECT 
-        (SELECT IFNULL(SUM(amount),0) FROM Revenue WHERE user_id = ?) - 
-        (SELECT IFNULL(SUM(amount),0) FROM Expenses WHERE user_id = ?) AS balance");
+        (SELECT IFNULL(SUM(revenue_amount),0) FROM revenue WHERE user_id = ? AND sub_user_id is NULL) - 
+        (SELECT IFNULL(SUM(expense_amount),0) FROM expenses WHERE user_id = ? AND sub_user_id is NULL ) AS balance");
 $stmt->bind_param("ii", $user_id, $user_id);
 $stmt->execute();
+if ($stmt->errno) {
+    echo "Error executing query: " . $stmt->error;
+}
+
 $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
@@ -18,9 +23,9 @@ if ($result->num_rows > 0) {
     $_SESSION["error_message"] = "No balance found for the user.";
 }
 
-$stmt = $conn->prepare("SELECT SUM((SELECT IFNULL(SUM(amount),0) FROM Revenue WHERE user_id = sub_user_id) - 
-    (SELECT IFNULL(SUM(amount),0) FROM Expenses WHERE user_id = sub_user_id)) AS balance FROM sub_users WHERE user_id = ?");
-$stmt->bind_param("i", $user_id);
+$stmt = $conn->prepare("SELECT SUM((SELECT IFNULL(SUM(revenue_amount),0) FROM Revenue WHERE user_id = ? AND sub_user_id IS NOT NULL) - 
+(SELECT IFNULL(SUM(expense_amount),0) FROM Expenses WHERE user_id = ? AND sub_user_id IS NOT NULL)) AS balance FROM sub_users WHERE user_id = ?");
+$stmt->bind_param("iii", $user_id, $user_id, $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
